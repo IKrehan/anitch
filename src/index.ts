7ae05443fcc5502bce8c71f9552c8f647c2b563e
Irvig Krehan <irvigkrehan@hotmail.com>
2021-11-22 19:47:08 -0300

    refactor: change main function

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import Scraping from './controllers/scraping';
 import Cli from './controllers/cli';
+import open from 'open';
+
 
 const scraping = new Scraping();
 const cli = new Cli();
 
-const run = async (): Promise<void> => {
+(async () => {
   const animeOptions = await scraping.search(await cli.getSearchQuery());
   const choosenAnime = await cli.chooseAnime(animeOptions);
   const ep = await cli.chooseEp(await scraping.getEps(choosenAnime));
-
   const videoUrls = await scraping.getEmbeddedVideo(ep);
-  console.log(videoUrls);
-};
 
-run();
+  /* by now its opening the bare url, in the future the objective is to open
+  the video in some kind of custom player, web or local. */
+  open(videoUrls[0].url);
+})();
